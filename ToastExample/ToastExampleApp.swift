d867c7e8e8e00d08c743283b286c0f921f645ee6
VuCuongHD96 <xuancuonghd1102@gmail.com>
2024-01-01 09:55:16 +0700

    Initial Commit

---
+++ ToastExample/ToastExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToastExampleApp.swift
+//  ToastExample
+//
+//  Created by Work on 01/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToastExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
